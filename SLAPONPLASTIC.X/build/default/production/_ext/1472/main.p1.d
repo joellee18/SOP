c8dbdae921ac1646d570da1e01bef22d62332973
leahhartwell <leahhartwell@email.kpu.ca>
2019-03-08 10:28:45 -0800

    original code for SOP given + osc.h/c changed: sumovore.c OSC setting was = INTIO67, got rid of the 6, so just INTIO7

---
+++ SLAPONPLASTIC.X/build/default/production/_ext/1472/main.p1.d
@@ -0,0 +1,6 @@
+ build/default/production/_ext/1472/main.d  \
+ build/default/production/_ext/1472/main.p1:  \
+ ../main.c  \
+../interrupts.h  \
+../sumovore.h  \
+../motor_control.h 
---
+++ SLAPONPLASTIC.X/build/default/production/_ext/1472/motor_control.p1.d
@@ -0,0 +1,5 @@
+ build/default/production/_ext/1472/motor_control.d  \
+ build/default/production/_ext/1472/motor_control.p1:  \
+ ../motor_control.c  \
+../sumovore.h  \
+../motor_control.h 
