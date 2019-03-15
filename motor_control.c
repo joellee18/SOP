e34c60280acd7b3001d0001f9dc4dca5fcb164ad
leahhartwell <leahhartwell@email.kpu.ca>
2019-03-15 11:39:22 -0700

    changes speeds forwards and backwards now straight put made default_settings func

--- motor_control.c
+++ motor_control.c
@@ -17,110 +17,11 @@ void straight_reverse_slow(void);
 void straight_reverse_medium(void);
 void straight_reverse_fast(void);
 
+void default_settings(void);
+
 void motor_control(void)
 {
-    OpenTimer0(TIMER_INT_OFF & T0_SOURCE_INT & T0_16BIT & T0_PS_1_256);
     
-    WriteTimer0(0);
-    TMR0IF = 0;
-    while(!TMR0IF)
-    {
-        straight_forward_fast();
-    }
-        
-    WriteTimer0(0);
-    TMR0IF = 0;
-    while(!TMR0IF)
-    {
-        straight_forward_medium();
-    }
-    
-    WriteTimer0(0);
-    TMR0IF = 0;
-    while(!TMR0IF)
-    {
-        straight_forward_slow();
-    }
-    
-    WriteTimer0(0);
-    TMR0IF = 0;
-    while(!TMR0IF)
-    {
-        straight_stop();
-    }
-    
-    WriteTimer0(0);
-    TMR0IF = 0;
-    while(!TMR0IF)
-    {
-        straight_reverse_slow();
-    }
-    
-    WriteTimer0(0);
-    TMR0IF = 0;
-    while(!TMR0IF)
-    {
-        straight_reverse_medium();
-    }
-    
-    WriteTimer0(0);
-    TMR0IF = 0;
-    while(!TMR0IF)
-    {
-        straight_reverse_fast();
-    }
-    
-    WriteTimer0(0);
-    TMR0IF = 0;
-    while(!TMR0IF)
-    {
-        straight_stop();
-    }
-    
-//    straight_forward_medium();
-//    for (int i = 0; i < 80 ;i++)
-//        _delay(100000ul);
-//
-//    straight_forward_slow();
-//    for (int i = 0; i < 80 ;i++)
-//        _delay(100000ul);
-//    
-//    straight_stop();
-//    for (int i = 0; i < 80 ;i++)
-//        _delay(100000ul);     
-//    
-//    straight_reverse_slow();
-//    for (int i = 0; i < 80 ;i++)
-//        _delay(100000ul);
-//    
-//    straight_reverse_medium();
-//    for (int i = 0; i < 80 ;i++)
-//        _delay(100000ul);
-//    
-//    straight_reverse_fast();
-//    for (int i = 0; i < 80 ;i++)
-//        _delay(100000ul);
-//    
-//    straight_stop();
-//    for (int i = 0; i < 80 ;i++)
-//        _delay(100000ul);
-    
-    // very simple motor control
-//     switch(SeeLine.B)
-//     {
-//        case 0b00100u:
-//        case 0b00010u:
-//        case 0b01000u:
-//        case 0b00001u:
-//        case 0b10000u:
-//                       //no breaks all above readings end up here
-//                       follow_simple_curves();
-//                       break;
-//        case 0b00000u:
-//                        motors_brake_all();
-//                       break;
-//        default:       break;
-//      } 
 }
 
 void follow_simple_curves(void)
@@ -163,20 +64,20 @@ void turn_right(void)
 
 void straight_forward_fast(void)
 {
-    set_motor_speed(left, fast, 0); 
+    set_motor_speed(left, fast, -100); 
     set_motor_speed(right, fast, 0);
 }
 
 void straight_forward_medium(void)
 {
     set_motor_speed(left, medium, -100); 
-    set_motor_speed(right, medium, -100);
+    set_motor_speed(right, medium, -120);
 }
 
 void straight_forward_slow(void)
 {
     set_motor_speed(left, slow, -100); 
-    set_motor_speed(right, slow, -100);
+    set_motor_speed(right, slow, -117);
 }
 
 void straight_stop(void)
@@ -188,17 +89,96 @@ void straight_stop(void)
 void straight_reverse_slow(void)
 {
     set_motor_speed(left, rev_slow, 100); 
-    set_motor_speed(right, rev_slow, 100);
+    set_motor_speed(right, rev_slow, 130);
 }
 
 void straight_reverse_medium(void)
 {
     set_motor_speed(left, rev_medium, 100); 
-    set_motor_speed(right, rev_medium, 100);
+    set_motor_speed(right, rev_medium, 127);
 }
 
 void straight_reverse_fast(void)
 {
     set_motor_speed(left, rev_fast, 0); 
     set_motor_speed(right, rev_fast, 0);
+}
+
+void default_settings(void)
+{
+    OpenTimer0(TIMER_INT_OFF & T0_SOURCE_INT & T0_16BIT & T0_PS_1_256);
+    
+    WriteTimer0(0);
+    TMR0IF = 0;
+    while(!TMR0IF)
+    {
+        straight_forward_fast();
+    }
+        
+    WriteTimer0(0);
+    TMR0IF = 0;
+    while(!TMR0IF)
+    {
+        straight_forward_medium();
+    }
+    
+    WriteTimer0(0);
+    TMR0IF = 0;
+    while(!TMR0IF)
+    {
+        straight_forward_slow();
+    }
+    
+    WriteTimer0(0);
+    TMR0IF = 0;
+    while(!TMR0IF)
+    {
+        straight_stop();
+    }
+    
+
+    WriteTimer0(0);
+    TMR0IF = 0;
+    while(!TMR0IF)
+    {
+        straight_reverse_slow();
+    }
+   
+    WriteTimer0(0);
+    TMR0IF = 0;
+    while(!TMR0IF)
+    {
+        straight_reverse_medium();
+    }
+    
+    WriteTimer0(0);
+    TMR0IF = 0;
+    while(!TMR0IF)
+    {
+        straight_reverse_fast();
+    }
+    
+    WriteTimer0(0);
+    TMR0IF = 0;
+    while(!TMR0IF)
+    {
+        straight_stop();
+    }
+    
+     very simple motor control
+     switch(SeeLine.B)
+     {
+        case 0b00100u:
+        case 0b00010u:
+        case 0b01000u:
+        case 0b00001u:
+        case 0b10000u:
+                       no breaks all above readings end up here
+                       follow_simple_curves();
+                       break;
+        case 0b00000u:
+                        motors_brake_all();
+                       break;
+        default:       break;
+      } 
 }
