03d75d5a446335d912e1efb63ba3bccaf9763aee
leahhartwell <leahhartwell@email.kpu.ca>
2019-03-14 17:07:06 -0700

    not working motor control

--- motor_control.c
+++ motor_control.c
@@ -10,22 +10,57 @@ void spin_right(void);
 
 void motor_control(void)
 {
-     // very simple motor control
-     switch(SeeLine.B)
-     {
-        case 0b00100u:
-        case 0b00010u:
-        case 0b01000u:
-        case 0b00001u:
-        case 0b10000u:
-                       //no breaks all above readings end up here
-                       follow_simple_curves();
-                       break;
-        case 0b00000u:
-                        motors_brake_all();
-                       break;
-        default:       break;
-      } 
+    set_motor_speed(left, fast, 0); 
+    set_motor_speed(right, fast, 0);
+    for (int i = 0; i>400 ;i++)
+        _delay(100000ul);
+
+    set_motor_speed(left, medium, 0); 
+    set_motor_speed(right, medium, 0);
+    for (int i = 0; i>400 ;i++)
+        _delay(100000ul);
+
+    set_motor_speed(left, slow, 0); 
+    set_motor_speed(right, slow, 0);
+    for (int i = 0; i>400 ;i++)
+        _delay(100000ul);
+
+    set_motor_speed(left, stop, 0); 
+    set_motor_speed(right, stop, 0);
+    for (int i = 0; i>400 ;i++)
+        _delay(100000ul);
+
+    set_motor_speed(left, rev_slow, 0); 
+    set_motor_speed(right, rev_slow, 0);
+    for (int i = 0; i>400 ;i++)
+        _delay(100000ul);
+
+    set_motor_speed(left, rev_medium, 0); 
+    set_motor_speed(right, rev_medium, 0);
+    for (int i = 0; i>400 ;i++)
+        _delay(100000ul);
+
+    set_motor_speed(left, rev_fast, 0); 
+    set_motor_speed(right, rev_fast, 0);
+    for (int i = 0; i>400 ;i++)
+        _delay(100000ul);
+     
+    // very simple motor control
+//     switch(SeeLine.B)
+//     {
+//        case 0b00100u:
+//        case 0b00010u:
+//        case 0b01000u:
+//        case 0b00001u:
+//        case 0b10000u:
+//                       //no breaks all above readings end up here
+//                       follow_simple_curves();
+//                       break;
+//        case 0b00000u:
+//                        motors_brake_all();
+//                       break;
+//        default:       break;
+//      } 
 }
 
 void follow_simple_curves(void)
