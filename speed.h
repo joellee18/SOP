1526265be172520706973bd8eb9121350a0f36a6
joellee18 <48364025+joellee18@users.noreply.github.com>
2019-04-06 11:42:03 -0700

    Create speed.h

---
+++ speed.h
@@ -0,0 +1,13 @@
+void spin_left(void);
+void turn_left(void);
+void straight_fwd(void);
+void turn_right(void);
+void spin_right(void);
+
+void straight_forward_fast(void);
+void straight_forward_medium(void);
+void straight_forward_slow(void);
+void straight_stop(void);
+void straight_reverse_slow(void);
+void straight_reverse_medium(void);
+void straight_reverse_fast(void);
