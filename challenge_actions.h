4cea324e1f416551aa806623b2a808dc63ad7182
joellee18 <48364025+joellee18@users.noreply.github.com>
2019-04-06 14:16:11 -0700

    Create challenge_actions.h

---
+++ challenge_actions.h
@@ -0,0 +1,9 @@
+void follow_simple_curves(void);
+void follow_right_angle_crossover(void);
+void follow_crossover(void);
+void follow_acute_angle_turn(void);
+void follow_landing_pad(void);
+void follow_gaps(void);
+
+unsigned char left_counter3(unsigned char left_value3);
+unsigned char right_counter3(unsigned char right_value3);
